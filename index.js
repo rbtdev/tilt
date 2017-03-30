8b788db5df84a8d2b92fbf43b26a423bd80c0547
Robert Thuleen <rob.thuleen@fuseintegration.com>
2017-03-30 15:37:51 -0700

    test

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 
-
-function tilt (tilt) {
+function showTilt (tilt) {
     var display= document.getElementById('tilt');
     display.innerHTML = JSON.stringify(tilt);
     
 }
 
+
 window.addEventListener("deviceorientation", function(event) 
 {
+
     var tilt = {
         x:Math.round(event.gamma),
         y:Math.round(event.beta),
         rot:Math.round(event.alpha),
     }
 
-    tilt(tilt)
+    showTilt(tilt)
 }, true);
